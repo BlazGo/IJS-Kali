e10b0591a4b6011972aa97279ef8f4d144e4e314
BlazGo <bg2286@student.uni-lj.si>
2021-01-14 20:01:59 +0100

    publish model tf

--- obj_calib/src/object_positioner.py
+++ obj_calib/src/object_positioner.py
@@ -6,54 +6,41 @@ Publish/initialize at the beginning and read from server.
 import roslib
 import rospy
 import tf
-from random import random
 
-x, y, z = 0, 0, 0
-
-class randPos():
+class modelTf():
     """
     random pos generator
     """
-    def __init__(self, x_range = (-0.5, 0.5),
-                y_range = (-0.5, 0.5),
-                z_range = (-0.2, 0.2)):
 
-        """
-        Maybe make a functionality to scale the workspace
-        """
+    def __init__(self, node_name="obj_calib", object_name="my_object", base_link="panda_link0"):
 
-        self.x_range = x_range
-        self.y_range = y_range
-        self.z_range = z_range
+        self.object_name = object_name
+        self.base_link = base_link
         
-        self.x = random() - 1
-        self.y = random() - 1
-        self.z = (random() - 1) * 0.2 
-        print("Initialised random class")
+        self.X_POS = 0.3
+        self.Y_POS = 0.3
+        self.Z_POS = 0
 
-    def generate_Newpos(self):
-        self.x = random() - 1
-        self.y = random() - 1
-        self.z = (random() - 1) * 0.2 
+        self.trans = [self.X_POS, self.Y_POS, self.Z_POS]
+        self.eulers = [0,0,0]
+
+        try:
+            rospy.init_node("object_positioner", anonymous=True)
+        except:
+            print("[WARN] Node already exists")
+
+        self.tfBroadcaster = tf.TransformBroadcaster()
+
+    def pub_tf(self):
+        self.tfBroadcaster.sendTransform(self.trans,
+                                        tf.transformations.quaternion_from_euler(self.eulers[0], self.eulers[1], self.eulers[2], axes="rzyx"),
+                                        rospy.Time.now(),
+                                        self.object_name,
+                                        self.base_link)
+
+if __name__ == "__main__":
+    mTf = modelTf()
+    while rospy.is_shutdown() == False:
+        mTf.pub_tf()
+        rospy.sleep(2)
         
-        print("generated random pos")
-
-    def get_pos(self):
-        print("Retrieved pos")
-        return self.x, self.y, self.z
-
-objectname = "my_object"
-rospy.init_node("object_positioner")
-broTf = tf.TransformBroadcaster()
-
-pos = randPos()
-trans = pos.get_pos()
-
-while not rospy.is_shutdown():
-    broTf.sendTransform(trans,
-                        tf.transformations.quaternion_from_euler(0,0,0),
-                        rospy.Time.now(),
-                        objectname,
-                        "panda_link0")
-
-    rospy.sleep(2)
